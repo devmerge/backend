0977778a6dce3e50b89f1bc875c13b1b634982b2
Raman Shalupau <raman.shalupau@gmail.com>
2013-12-20 01:11:34 +0800

    running a job to query for posts with #devmerge

--- app/jobs/updateMerges.js
+++ app/jobs/updateMerges.js
@@ -10,14 +10,36 @@ define([
 			if (err) throw err;
 			db.collection('users').find().toArray(function (err, users) {
 				_.each(users, function (user) {
-					// console.log(user);
 
 					if (!user.accessToken) return;
 
 					var fb = FB;
 					fb.setAccessToken(user.accessToken);
-					fb.api('/me/posts', function (err, data) {
-						console.log(data);
+					fb.api('/me/posts', function (data) {
+						if (data.error) return;
+
+						var posts = _.filter(data.data, function (post) {
+							return (!!post.message &&
+								post.message.indexOf('#devmerge') != -1 &&
+								!!post.place);
+						});
+
+						_.each(posts, function (post) {
+							db.collection('posts').update(
+								{ id: post.id },
+								post,
+								{ upsert: true },
+								function (err, qty) {
+									if (err) {
+										throw err;
+										return false;
+									} else {
+										console.log('Inserted:', qty);
+									}
+								}
+							);
+						});
+
 						// filter out only with message contains #devmerge
 					});
 				});
