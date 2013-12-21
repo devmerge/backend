cd930c218377ac587697050ff954846fa6ac3212
Raman Shalupau <raman.shalupau@gmail.com>
2013-12-21 18:08:34 +0800

    retrieving posts for only past 24 hours by default

--- app/controllers/checkins.js
+++ app/controllers/checkins.js
@@ -1,20 +1,28 @@
-define(['underscore', 'config/db', 'fb'],
-function (_, mongo, FB) {
+define(['underscore', 'config/db', 'fb', 'moment'],
+function (_, mongo, FB, moment) {
 
 	var Actions = {};
 
 	Actions.list = function (req, res) {
-		var params = req.query || {};
+		var params = req.query || {},
+			past24hours = moment().subtract('days', 1).utc().format(),
+			criteria = {
+				"created_time": {
+					$gte: past24hours
+				}
+			};
+
+		if (params.all) {
+			criteria = {};
+		}
 
 		mongo(function (err, db) {
 			if (err) throw err;
-			db.collection('posts').find().toArray(function (err, checkins) {
+			db.collection('posts').find(criteria).toArray(function (err, checkins) {
 				if (err) throw err;
-
 				_.each(checkins, function (checkin) {
 					// checkin = _.omit(checkin, 'secret', 'info');
 				})
-
 				res.send(checkins);
 			});
 		});
